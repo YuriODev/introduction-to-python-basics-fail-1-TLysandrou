3c3a84f9400dfc44ba5daf3e603863c42dd44753
TLysandrou <158465557+TLysandrou@users.noreply.github.com>
2024-03-25 12:30:19 +0000

    exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,7 @@
 # Exercise 1
 # Your solution comes here
+number = int(input("Enter a number: "))
+
+part1 = (number // 10000) + ((number // 100) % 10) + (number % 10)
+part2 = ((number // 1000) % 10) + ((number // 10) % 10)
+print(f"Number: {part1}{part2}")
